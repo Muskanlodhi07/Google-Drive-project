b4d21130d978b8a86c5b465f6f36eadd78842fec
Muskanlodhi07 <y226101034@dhsgsu.edu.in>
2025-08-12 15:52:18 +0530

    Porting changed

--- app.js
+++ app.js
@@ -21,8 +21,8 @@ app.use("/user" , userRouter);
 app.use(express.static('public'));
 
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`🚀 Server running on http://localhost:${PORT}`);
+app.listen(process.env.PORT || 3000, () => {
+  console.log(`Server running on port ${process.env.PORT || 3000}`);
 });
+
 
