6da8e2e55b2af9a3f9e1ef4c2e1b4a919c197d76
Muskanlodhi07 <y226101034@dhsgsu.edu.in>
2025-08-12 16:00:12 +0530

    Deploy ready

--- app.js
+++ app.js
@@ -21,8 +21,8 @@ app.use("/user" , userRouter);
 app.use(express.static('public'));
 
 
-app.listen(process.env.PORT || 3000, () => {
-  console.log(`Server running on port ${process.env.PORT || 3000}`);
-});
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
 
 
